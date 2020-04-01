69d5a40697a017f798fc44bb9e550048ba692d77
TemirT27 <temir9691@mail.ru>
2020-04-01 11:44:38 +0600

    w11

---
+++ week11/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\madina\desktop\fourth~1\webdev~1\week11\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
